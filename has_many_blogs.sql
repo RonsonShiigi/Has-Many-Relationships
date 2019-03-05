1566ee2babe5b6dee3321ef12f44ded9cf72c187
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2019-03-04 23:56:53 -1000

    added data into schema

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,7 +1,12 @@
 DROP USER IF EXISTS has_many_user;
 DROP DATABASE IF EXISTS has_many_blogs;
+
+
 CREATE USER has_many_user;
 CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
+
+\c has_many_blogs;
+
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS comments;
@@ -15,26 +20,29 @@ CREATE TABLE users(
     last_name character varying(90) NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP NOT NULL DEFAULT now()
-);
+    );
+
+
 
 CREATE TABLE posts(
     ID SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(ID),
     title character varying(180) NULL,
     url character varying(510) NULL,
     content TEXT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP NOT NULL DEFAULT now()
-);
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    user_id INTEGER NOT NULL REFERENCES users(ID)
+    );
 
 
 CREATE TABLE comments(
     ID SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(ID),
-    post_id INTEGER REFERENCES posts(ID),
     body character varying(510) NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP NOT NULL DEFAULT now()
-);
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    post_id INTEGER REFERENCES posts(ID),
+    user_id INTEGER NOT NULL REFERENCES users(ID)
+    );
 
-SELECT * FROM comments;
+
+\i scripts/blog_data.sql;
